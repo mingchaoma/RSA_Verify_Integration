4a2828df196626da4897bb97c25b60de5255c624
Mingchao Ma <mingchao_ma@hotmail.com>
2020-12-15 16:04:36 +0000

    Update RSA_Verify.js
    Catch Lookup API error added

--- functions/RSA_Verify.js
+++ functions/RSA_Verify.js
@@ -1,51 +1,55 @@
-exports.handler = function(context, event, callback) {
-    console.log(event.To);
-    console.log(event.Customcode);
-    console.log(event.Channel);
-    
-    if (event.verify_sid !== context.VERIFY_SID || event.api_secret !== context.API_SECRET) {
-        //authentication failure
-        let response=new Twilio.Response();
-        response.setStatusCode(401);
-        response.setBody("Unauthorized");
-        callback(null, response);
-    }else 
-    {
-        var client=context.getTwilioClient();
-        var response=new Twilio.Response();
-        //call Lookup API to convert phone number to E.164 format
-         client.lookups.phoneNumbers(event.To)
-            .fetch()
-            .then ((phone_number)=>{
-                var to=phone_number.phoneNumber;
-                //environment variables
-                var verify_sid=context.VERIFY_SID;
-                //http request parameters
-                var channel=event.Channel;
-                var customcode=event.Customcode;
-                var custommessage=event.Custommessage
-                //call Verify API
-                client.verify.services(verify_sid)
-                    .verifications
-                    .create({
-                        to:to,
-                        customCode: customcode,
-                        customMessage:custommessage,
-                        channel:channel
-                        })
-                    .then((verification) => {
-                        //console.log(verification);
-                        response.appendHeader('Content-Type', 'application/json');
-                        response.setBody(verification);
-                        callback(null, response);
-                        })
-                    .catch ((error)=>{
-                        console.log ("Error: " + error);
-                        callback (error);
-                    })
-        });
-          
-        
-    }
-    
-}
+exports.handler = function (context, event, callback) {
+  console.log(event.To);
+  console.log(event.Customcode);
+  console.log(event.Channel);
+
+  if (
+    event.verify_sid !== context.VERIFY_SID ||
+    event.api_secret !== context.API_SECRET
+  ) {
+    //authentication failure
+    let response = new Twilio.Response();
+    response.setStatusCode(401);
+    response.setBody("Unauthorized");
+    callback(null, response);
+  } else {
+    var client = context.getTwilioClient();
+    var response = new Twilio.Response();
+    //call Lookup API to convert phone number to E.164 format
+    client.lookups
+      .phoneNumbers(event.To)
+      .fetch()
+      .then((phone_number) => {
+        var to = phone_number.phoneNumber;
+        //environment variables
+        var verify_sid = context.VERIFY_SID;
+        //http request parameters
+        var channel = event.Channel;
+        var customcode = event.Customcode;
+        var custommessage = event.Custommessage;
+        //call Verify API
+        client.verify
+          .services(verify_sid)
+          .verifications.create({
+            to: to,
+            customCode: customcode,
+            customMessage: custommessage,
+            channel: channel,
+          })
+          .then((verification) => {
+            //console.log(verification);
+            response.appendHeader("Content-Type", "application/json");
+            response.setBody(verification);
+            callback(null, response);
+          })
+          .catch((error) => {
+            console.log("Error: " + error);
+            callback(error);
+          });
+      })
+      .catch((error) => {
+        console.log("Error: " + error);
+        callback(error);
+      });
+  }
+};
